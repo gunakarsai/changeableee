df93e450dba50e99c5674e7091696e5b38112397
gunakarsai <30504527+gunakarsai@users.noreply.github.com>
2020-02-05 07:11:35 +0530

    Add files via upload
    commited

---
+++ AtosDiscount/src/com/atos/model/Discounts.java
@@ -0,0 +1,54 @@
+package com.atos.model;
+
+public class Discounts {
+	private int product_id;
+	private int product_price;
+	private int product_discount;
+	private String Discount_valid;
+	private String product_type;
+	
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public int getProduct_discount() {
+		return product_discount;
+	}
+	public void setProduct_discount(int product_discount) {
+		this.product_discount = product_discount;
+	}
+	public Discounts() {
+		super();
+	}
+	public String getDiscount_valid() {
+		return Discount_valid;
+	}
+	public void setDiscount_valid(String discount_valid) {
+		Discount_valid = discount_valid;
+	}
+	public String getProduct_type() {
+		return product_type;
+	}
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+	
+	public Discounts(int product_id, int product_price, int product_discount, String discount_valid, String product_type) {
+		super();
+		this.product_id = product_id;
+		this.product_price = product_price;
+		this.product_discount = product_discount;
+		this.Discount_valid = discount_valid;
+		this.product_type=product_type;
+	}
+	
+	
+}
